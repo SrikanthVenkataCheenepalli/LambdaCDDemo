faf9b1b8187f078cea3d4d104362a78974203984
Srikanthcvsk <cvsk.jobs@gmail.com>
2018-11-30 11:41:48 +0100

    CodePipelineTest

--- LambdaCode/Lambda_fn.py
+++ LambdaCode/Lambda_fn.py
@@ -5,6 +5,6 @@ ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
     print("Big decisions")
-    print(" Georg ")
+    print(" Testing Code Pipeline")
     
     return {"statusCode": 200, "body": json.dumps(response)}
